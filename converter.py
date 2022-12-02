600ecc82cce94c6e2798d31dd54c44cced7ed7e8
Happitack <70133275+Happitack@users.noreply.github.com>
2022-12-02 11:24:29 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -3,7 +3,6 @@ from sense_hat import SenseHat
 sh = SenseHat()
 off = (0, 0, 0)
 
-
 def binary(time, colrow, direction, color):
     '''Takes a time format, a collumn/row number, a directional variable and a color value'''
     binary_str = "{0:8b}".format(time)
@@ -18,4 +17,3 @@ def binary(time, colrow, direction, color):
                     sh.set_pixel(colrow, v, off)
                 else:
                     sh.set_pixel(v, colrow, off)
-
